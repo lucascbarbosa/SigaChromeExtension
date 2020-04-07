412eb08a167b36585e5dbea836e2683d6537dae3
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-04-07 02:11:43 -0300

    added the eventSave script

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "content.js"});
-  
+  chrome.tabs.executeScript(null, { file: "content.js" });
+  chrome.tabs.executeScript(null, { file: "eventSave.js" });
 });
