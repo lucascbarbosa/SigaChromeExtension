b7639281ba8d403d14ac2f9db31e8df110a0e1aa
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2020-04-07 04:50:50 -0300

    Delete eventSave.js

--- eventSave.js
+++
@@ -1,7 +0,0 @@
-function clickSave() {
-    var xpath = "//*[@class='l4V7wb Fxmcue']//span[contains(text(), 'Salvar')]";
-    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    matchingElement.click();
-}
-
-clickSave();
