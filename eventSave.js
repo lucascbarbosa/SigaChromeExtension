8eadbd945743bea1a714c20220af61b85c9f7d83
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2020-04-07 02:09:11 -0300

    added clickSave()

--- eventSave.js
+++ eventSave.js
@@ -1,8 +1,7 @@
-
 function clickSave() {
-    console.log("acessei o site");
     var xpath = "//*[@class='l4V7wb Fxmcue']//span[contains(text(), 'Salvar')]";
     var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     matchingElement.click();
-    
 }
+
+clickSave();
